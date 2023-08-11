d2bf5ae4ab01083166bf96b056b0e04d32c7dbe3
Shreya Parkar <shreyaparker317@gmail.com>
2023-08-11 20:46:37 +0530

    Displaying All Pins For Homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,36 @@
+"use client"
+import {collection, getDocs, getFirestore, query} from "firebase/firestore";
+import {useEffect, useState} from "react";
+import app from "@/app/firebase/firebaseConfig";
+import PinList from "@/app/components/pins/PinList";
 
 
 export default function Home() {
-  return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    const db=getFirestore(app);
+    const [listOfPins,setListOfPins]=useState([]);
+    useEffect(()=>{
+        getAllPins();
+    },[])
 
-      </main>
+
+    const getAllPins=async()=>{
+        setListOfPins([])
+        const q=query(collection(db,'pinterest-post'))
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+
+            setListOfPins(listOfPins=>[...listOfPins,doc.data()]);
+            console.log(listOfPins)
+        });
+    }
+  return (
+      <>
+          <div className="p-3">
+              <PinList
+                  listOfPins={listOfPins}
+              />
+
+          </div>
+      </>
   )
 }
